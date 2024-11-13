cfaca46e49306fa7c70a53c3d10f694e07f77019
Darakhshan08 <darakhshanshah8@gmail.com>
2024-11-13 15:16:53 +0500

    update
    1

--- React3/src/component/FooterBottum.jsx
+++ React3/src/component/FooterBottum.jsx
@@ -13,7 +13,8 @@ function FooterBottum() {
             href="https://flowbite.com"
             src="https://flowbite.com/docs/images/logo.svg"
             alt="Flowbite Logo"
-            name="Flowbite"
+            name="Darakhshan"
+   
           />
         </div>
         <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
